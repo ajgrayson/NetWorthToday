baf80b43c4feb7b3826d19644a9d2867063a8bf3
John Grayson <johnathangrayson@Johnathans-MacBook-Pro-3.local>
2014-12-02 20:59:08 +1300

    Setup initial project
    Started work on the main layout

---
+++ NetWorthToday/Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Bridging-Header.h
+//  NetWorthToday
+//
+//  Created by Johnathan Grayson on 2/12/14.
+//  Copyright (c) 2014 Johnathan Grayson. All rights reserved.
+//
+
+#import <CouchbaseLite/CouchbaseLite.h>
+
+#import <FacebookSDK/FacebookSDK.h>
