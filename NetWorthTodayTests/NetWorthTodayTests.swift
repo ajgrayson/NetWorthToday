292592234f66657b60a9cbbe0fdd44571224ab6d
John Grayson <johnathangrayson@Johnathans-MacBook-Pro-3.local>
2014-12-02 20:29:52 +1300

    Initial Commit

---
+++ NetWorthTodayTests/NetWorthTodayTests.swift
@@ -0,0 +1,36 @@
+//
+//  NetWorthTodayTests.swift
+//  NetWorthTodayTests
+//
+//  Created by Johnathan Grayson on 2/12/14.
+//  Copyright (c) 2014 Johnathan Grayson. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NetWorthTodayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
