223dae84f4cd76193dd33ae6a31d684c9a874b47
John Grayson <johnathangrayson@Johnathans-MacBook-Pro-3.local>
2014-12-07 12:19:07 +1300

    Got the titles working on the lists

--- NetWorthToday/MemberOverviewViewController.swift
+++ NetWorthToday/MemberOverviewViewController.swift
@@ -20,6 +20,20 @@ class MemberOverviewViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier != "manageAssets" && segue.identifier != "manageLiabilities"){
+            return
+        }
+        
+        var nvc : UINavigationController = segue.destinationViewController as UINavigationController
+        var lvc : ItemListTableViewController = nvc.topViewController as ItemListTableViewController
+        
+        if(segue.identifier == "manageAssets") {
+            lvc.itemType = ItemType.Asset
+        } else {
+            lvc.itemType = ItemType.Liability
+        }
+    }
 
 }
 
